5595fbafc929f2ccca15898bc173e8c87c0e0faa
Ivan Rodriguez <irg0031@g.educaand.es>
2026-03-24 11:03:34 +0100

    24/03

---
+++ comandos.java
@@ -0,0 +1,23 @@
+public class GitComandos {
+    public static void main(String[] args) {
+        System.out.println(
+            "git init\n" +
+            "git clone\n" +
+            "git add\n" +
+            "git commit\n" +
+            "git status\n" +
+            "git branch\n" +
+            "git switch\n" +
+            "git checkout\n" +
+            "git merge\n" +
+            "git fetch\n" +
+            "git push\n" +
+            "git pull\n" +
+            "git rebase\n" +
+            "git log\n" +
+            "README.md\n" +
+            "markdown\n" +
+            ".git"
+        );
+    }
+}
